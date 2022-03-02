b2e3f0f0e82c4dcd878edb936ca88721ab024279
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-03-02 15:35:07 -0700

    implement integer operations, display update on operand selection

--- script.js
+++ script.js
@@ -1,111 +1,224 @@
-console.log("Hi, Athma!");
+console.log(`Hi, Athma!`);
 function preciseStr(n) {
-    var sigDig = 9;
+    let sigDig = 9;
     return n % 1 === 0 ? n.toString() : n.toPrecision(sigDig).toString();
 }
-function addition(n1, n2, fn) {
-    if (fn === void 0) { fn = preciseStr; }
-    return fn(n1 + n2);
+function addition(_state, fn) {
+    const { num1, num2 } = _state;
+    const sum = Number(num1) + Number(num2);
+    return fn(sum);
 }
-function subtraction(n1, n2, fn) {
-    if (fn === void 0) { fn = preciseStr; }
-    return fn(n1 - n2);
+function subtraction(_state, fn) {
+    const { num1, num2 } = _state;
+    const subtract = Number(num1) - Number(num2);
+    return fn(subtract);
 }
-function multiplication(n1, n2, fn) {
-    if (fn === void 0) { fn = preciseStr; }
-    return fn(n1 * n2);
+function multiplication(_state, fn) {
+    const { num1, num2 } = _state;
+    const mult = Number(num1) * Number(num2);
+    return fn(mult);
 }
-function division(n1, n2, fn) {
-    if (fn === void 0) { fn = preciseStr; }
-    return fn(n1 / n2);
+function division(_state, fn) {
+    const { num1, num2 } = _state;
+    const divide = Number(num2) === 0 ? 'lmao' : Number(num1) * Number(num2);
+    return fn(divide);
 }
-var state = {
+const display = document.getElementById('display');
+const state = {
     num1: '',
     num2: '',
+    total: '',
     isNum1Negative: false,
     isNum2Negative: false,
-    isDecimal: false,
+    isFraction: true,
+    isNextNumber: false,
     isAdd: false,
     isSubtract: false,
     isMultiply: false,
-    isDivide: false
+    isDivide: false,
 };
-var zero = document.querySelector('.zero');
-zero === null || zero === void 0 ? void 0 : zero.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '0') : (state.num2 += '0');
-    state.isDecimal = false;
+const zero = document.querySelector('.zero');
+zero?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '0';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '0';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var one = document.querySelector('.one');
-one === null || one === void 0 ? void 0 : one.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '1') : (state.num2 += '1');
-    state.isDecimal = false;
+const one = document.querySelector('.one');
+one?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '1';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '1';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var two = document.querySelector('.two');
-two === null || two === void 0 ? void 0 : two.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '2') : (state.num2 += '2');
-    state.isDecimal = false;
+const two = document.querySelector('.two');
+two?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '2';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '2';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var three = document.querySelector('.three');
-three === null || three === void 0 ? void 0 : three.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '3') : (state.num2 += '3');
-    state.isDecimal = false;
+const three = document.querySelector('.three');
+three?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '3';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '3';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var four = document.querySelector('.four');
-four === null || four === void 0 ? void 0 : four.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '4') : (state.num2 += '4');
-    state.isDecimal = false;
+const four = document.querySelector('.four');
+four?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '4';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '4';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var five = document.querySelector('.five');
-five === null || five === void 0 ? void 0 : five.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '5') : (state.num2 += '5');
-    state.isDecimal = false;
+const five = document.querySelector('.five');
+five?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '5';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '5';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var six = document.querySelector('.six');
-six === null || six === void 0 ? void 0 : six.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '6') : (state.num2 += '6');
-    state.isDecimal = false;
+const six = document.querySelector('.six');
+six?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '6';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '6';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var seven = document.querySelector('.seven');
-seven === null || seven === void 0 ? void 0 : seven.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '7') : (state.num2 += '7');
-    state.isDecimal = false;
+const seven = document.querySelector('.seven');
+seven?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '7';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '7';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var eight = document.querySelector('.eight');
-eight === null || eight === void 0 ? void 0 : eight.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '8') : (state.num2 += '8');
-    state.isDecimal = false;
+const eight = document.querySelector('.eight');
+eight?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '8';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '8';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var nine = document.querySelector('.nine');
-nine === null || nine === void 0 ? void 0 : nine.addEventListener('click', function () {
-    !state.isDecimal ? (state.num1 += '9') : (state.num2 += '9');
-    state.isDecimal = false;
+const nine = document.querySelector('.nine');
+nine?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '9';
+        display.textContent = state.num1;
+    }
+    else {
+        state.num2 += '9';
+        display.textContent = state.num2;
+    }
+    state.isNextNumber = false;
 });
-var dot = document.querySelector('.dot');
-dot === null || dot === void 0 ? void 0 : dot.addEventListener('click', function () {
-    state.isDecimal = true;
+const dot = document.querySelector('.dot');
+dot?.addEventListener('click', () => {
+    state.isFraction ? (state.isNextNumber = false) : (state.isNextNumber = true);
+    state.num1 += '.';
+    state.isFraction = false;
 });
-var clear = document.querySelector('.clear');
-clear === null || clear === void 0 ? void 0 : clear.addEventListener('click', function () {
+const clear = document.querySelector('.clear');
+clear?.addEventListener('click', () => {
     window.location.reload();
 });
-var plus = document.querySelector('.plus');
-plus === null || plus === void 0 ? void 0 : plus.addEventListener('click', function () {
+const plus = document.querySelector('.plus');
+plus?.addEventListener('click', () => {
     state.isAdd = true;
+    state.isNextNumber = true;
 });
-var subtract = document.querySelector('.minus');
-subtract === null || subtract === void 0 ? void 0 : subtract.addEventListener('click', function () {
+const subtract = document.querySelector('.minus');
+subtract?.addEventListener('click', () => {
     state.isSubtract = true;
+    state.isNextNumber = true;
 });
-var mult = document.querySelector('.multiply');
-mult === null || mult === void 0 ? void 0 : mult.addEventListener('click', function () {
+const mult = document.querySelector('.multiply');
+mult?.addEventListener('click', () => {
     state.isMultiply = true;
+    state.isNextNumber = true;
 });
-var divide = document.querySelector('.divide');
-divide === null || divide === void 0 ? void 0 : divide.addEventListener('click', function () {
+const divide = document.querySelector('.divide');
+divide?.addEventListener('click', () => {
     state.isDivide = true;
+    state.isNextNumber = true;
 });
-var plusMinus = document.querySelector('.plusMinus');
-plusMinus === null || plusMinus === void 0 ? void 0 : plusMinus.addEventListener('click', function () { });
-var percent = document.querySelector('.percent');
-percent === null || percent === void 0 ? void 0 : percent.addEventListener('click', function () { });
-console.log(state);
+const plusMinus = document.querySelector('.plusMinus');
+plusMinus?.addEventListener('click', () => { });
+const percent = document.querySelector('.percent');
+percent?.addEventListener('click', () => { });
+const equal = document.querySelector('.equal');
+equal?.addEventListener('click', () => {
+    switch (true) {
+        case state.isAdd:
+            state.total += addition(state, preciseStr);
+            break;
+        case state.isSubtract:
+            state.total += subtraction(state, preciseStr);
+            break;
+        case state.isMultiply:
+            state.total += multiplication(state, preciseStr);
+            break;
+        case state.isDivide:
+            state.total += division(state, preciseStr);
+            break;
+        default:
+            console.log("shouldn't see this...");
+    }
+    display.textContent = state.total;
+    state.num1 = '';
+    state.num2 = '';
+    state.total = '';
+    state.isNum1Negative = false;
+    state.isNum2Negative = false;
+    state.isFraction = true;
+    state.isNextNumber = false;
+    state.isAdd = false;
+    state.isSubtract = false;
+    state.isMultiply = false;
+    state.isDivide = false;
+});
--- script.ts
+++ script.ts
@@ -5,28 +5,40 @@ function preciseStr(n: number) {
 	return n % 1 === 0 ? n.toString() : n.toPrecision(sigDig).toString()
 }
 
-function addition(n1: number, n2: number, fn: Function = preciseStr) {
-	return fn(n1 + n2)
+function addition(_state: typeof state, fn: Function) {
+	const { num1, num2 } = _state
+	const sum = Number(num1) + Number(num2)
+	return fn(sum)
 }
 
-function subtraction(n1: number, n2: number, fn: Function = preciseStr) {
-	return fn(n1 - n2)
+function subtraction(_state: typeof state, fn: Function) {
+	const { num1, num2 } = _state
+	const subtract = Number(num1) - Number(num2)
+	return fn(subtract)
 }
 
-function multiplication(n1: number, n2: number, fn: Function = preciseStr) {
-	return fn(n1 * n2)
+function multiplication(_state: typeof state, fn: Function) {
+	const { num1, num2 } = _state
+	const mult = Number(num1) * Number(num2)
+	return fn(mult)
 }
 
-function division(n1: number, n2: number, fn: Function = preciseStr) {
-	return fn(n1 / n2)
+function division(_state: typeof state, fn: Function) {
+	const { num1, num2 } = _state
+	const divide = Number(num2) === 0 ? 'lmao' : Number(num1) * Number(num2)
+	return fn(divide)
 }
+
+const display = document.getElementById('display')
 
 const state = {
 	num1: '',
 	num2: '',
+	total: '',
 	isNum1Negative: false,
 	isNum2Negative: false,
-	isDecimal: false,
+	isFraction: true,
+	isNextNumber: false,
 	isAdd: false,
 	isSubtract: false,
 	isMultiply: false,
@@ -35,67 +47,139 @@ const state = {
 
 const zero = document.querySelector('.zero')
 zero?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '0') : (state.num2 += '0')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '0'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '0'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const one = document.querySelector('.one')
 one?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '1') : (state.num2 += '1')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '1'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '1'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const two = document.querySelector('.two')
 two?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '2') : (state.num2 += '2')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '2'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '2'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const three = document.querySelector('.three')
 three?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '3') : (state.num2 += '3')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '3'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '3'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const four = document.querySelector('.four')
 four?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '4') : (state.num2 += '4')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '4'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '4'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const five = document.querySelector('.five')
 five?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '5') : (state.num2 += '5')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '5'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '5'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const six = document.querySelector('.six')
 six?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '6') : (state.num2 += '6')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '6'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '6'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const seven = document.querySelector('.seven')
 seven?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '7') : (state.num2 += '7')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '7'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '7'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const eight = document.querySelector('.eight')
 eight?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '8') : (state.num2 += '8')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '8'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '8'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const nine = document.querySelector('.nine')
 nine?.addEventListener('click', () => {
-	!state.isDecimal ? (state.num1 += '9') : (state.num2 += '9')
-	state.isDecimal = false
+	if (!state.isNextNumber) {
+		state.num1 += '9'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '9'
+		display.textContent = state.num2
+	}
+
+	state.isNextNumber = false
 })
 
 const dot = document.querySelector('.dot')
 dot?.addEventListener('click', () => {
-	state.isDecimal = true
+	state.isFraction ? (state.isNextNumber = false) : (state.isNextNumber = true)
+	state.num1 += '.'
+	state.isFraction = false
 })
 
 const clear = document.querySelector('.clear')
@@ -106,21 +190,25 @@ clear?.addEventListener('click', () => {
 const plus = document.querySelector('.plus')
 plus?.addEventListener('click', () => {
 	state.isAdd = true
+	state.isNextNumber = true
 })
 
 const subtract = document.querySelector('.minus')
 subtract?.addEventListener('click', () => {
 	state.isSubtract = true
+	state.isNextNumber = true
 })
 
 const mult = document.querySelector('.multiply')
 mult?.addEventListener('click', () => {
 	state.isMultiply = true
+	state.isNextNumber = true
 })
 
 const divide = document.querySelector('.divide')
 divide?.addEventListener('click', () => {
 	state.isDivide = true
+	state.isNextNumber = true
 })
 
 const plusMinus = document.querySelector('.plusMinus')
@@ -129,4 +217,35 @@ plusMinus?.addEventListener('click', () => {})
 const percent = document.querySelector('.percent')
 percent?.addEventListener('click', () => {})
 
-console.log(state)
+const equal = document.querySelector('.equal')
+equal?.addEventListener('click', () => {
+	switch (true) {
+		case state.isAdd:
+			state.total += addition(state, preciseStr)
+			break
+		case state.isSubtract:
+			state.total += subtraction(state, preciseStr)
+			break
+		case state.isMultiply:
+			state.total += multiplication(state, preciseStr)
+			break
+		case state.isDivide:
+			state.total += division(state, preciseStr)
+			break
+		default:
+			console.log("shouldn't see this...")
+	}
+
+	display.textContent = state.total
+	state.num1 = ''
+	state.num2 = ''
+	state.total = ''
+	state.isNum1Negative = false
+	state.isNum2Negative = false
+	state.isFraction = true
+	state.isNextNumber = false
+	state.isAdd = false
+	state.isSubtract = false
+	state.isMultiply = false
+	state.isDivide = false
+})
