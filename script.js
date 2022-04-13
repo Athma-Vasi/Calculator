82d52e19a1b22d73b6bd08ca5f3ff6e7ead9d9bb
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-04-13 17:58:40 -0600

    freeze prev attempt

--- script.js
+++ script.js
@@ -4,38 +4,31 @@ function preciseStr(n) {
     return n % 1 === 0 ? n.toString() : n.toPrecision(sigDig).toString();
 }
 function addition(_state, fn) {
-    const { num1, num2 } = _state;
-    const sum = Number(num1) + Number(num2);
+    const { num1, total } = _state;
+    const sum = Number(total) + Number(num1);
     return fn(sum);
 }
 function subtraction(_state, fn) {
-    const { num1, num2 } = _state;
-    const subtract = Number(num1) - Number(num2);
+    const { num1, total } = _state;
+    const subtract = Number(total) - Number(num1);
     return fn(subtract);
 }
 function multiplication(_state, fn) {
-    const { num1, num2 } = _state;
-    const mult = Number(num1) * Number(num2);
+    const { num1, total } = _state;
+    const mult = Number(total) * Number(num1);
     return fn(mult);
 }
 function division(_state, fn) {
-    const { num1, num2 } = _state;
-    const divide = Number(num2) === 0 ? 'lmao' : Number(num1) / Number(num2);
+    const { num1, total } = _state;
+    const divide = Number(num1) === 0 || Number(total) === 0 ? '😲 😭' : Number(total) / Number(num1);
     return fn(divide);
 }
 const display = document.getElementById('display');
 const state = {
     num1: '',
-    num2: '',
     total: '',
-    isNum1Negative: false,
-    isNum2Negative: false,
     isFirstDecimal: true,
-    isNextNumber: false,
-    isAdd: false,
-    isSubtract: false,
-    isMultiply: false,
-    isDivide: false,
+    isFirstOperator: true,
 };
 const zero = document.querySelector('.zero');
 zero?.addEventListener('click', () => { });
@@ -56,74 +49,119 @@ seven?.addEventListener('click', () => { });
 const eight = document.querySelector('.eight');
 eight?.addEventListener('click', () => { });
 const nine = document.querySelector('.nine');
-nine?.addEventListener('click', () => { });
+nine?.addEventListener('click', () => {
+    state.num1 += '9';
+});
 const dot = document.querySelector('.dot');
-dot?.addEventListener('click', () => { });
-const clear = document.querySelector('.clear');
-clear?.addEventListener('click', () => {
-    window.location.reload();
+dot?.addEventListener('click', () => {
+    if (state.isFirstDecimal) {
+        state.num1 += '.';
+        state.isFirstDecimal = false;
+    }
 });
 const plus = document.querySelector('.plus');
-plus?.addEventListener('click', () => { });
+plus?.addEventListener('click', () => {
+    if (state.num1 === '.') {
+        display.textContent = '😲 😭';
+    }
+    else {
+        state.total = addition(state, preciseStr);
+    }
+    state.num1 = '';
+});
 const subtract = document.querySelector('.minus');
-subtract?.addEventListener('click', () => { });
+subtract?.addEventListener('click', () => {
+    if (state.num1 === '.') {
+        display.textContent = '😲 😭';
+    }
+    else {
+        state.total = subtraction(state, preciseStr);
+    }
+    state.num1 = '';
+});
 const mult = document.querySelector('.multiply');
-mult?.addEventListener('click', () => { });
+mult?.addEventListener('click', () => {
+    if (state.num1 === '.') {
+        display.textContent = '😲 😭';
+    }
+    else {
+        state.total = multiplication(state, preciseStr);
+    }
+    state.num1 = '';
+});
 const divide = document.querySelector('.divide');
-divide?.addEventListener('click', () => { });
+divide?.addEventListener('click', () => {
+    if (state.num1 === '.') {
+        display.textContent = '😲 😭';
+    }
+    else {
+        state.total = division(state, preciseStr);
+    }
+    state.num1 = '';
+});
 const plusMinus = document.querySelector('.plusMinus');
 plusMinus?.addEventListener('click', () => { });
 const percent = document.querySelector('.percent');
 percent?.addEventListener('click', () => { });
 const equal = document.querySelector('.equal');
 equal?.addEventListener('click', () => { });
-// const zero = document.querySelector('.zero')
-// zero?.addEventListener('click', () => {
-// if (!state.isNextNumber) {
-// state.num1 += '0'
-// display.textContent = state.num1
-// } else {
-// state.num2 += '0'
-// display.textContent = state.num2
-// }
-//
-// state.isFirstDecimal = true
-// state.isNextNumber = false
-// })
-// const plus = document.querySelector('.plus')
-// plus?.addEventListener('click', () => {
-// 	state.isAdd = true
-// 	state.isNextNumber = true
-// 	state.isFirstDecimal = true
-// })
-// const equal = document.querySelector('.equal')
-// equal?.addEventListener('click', () => {
-// 	switch (true) {
-// 		case state.isAdd:
-// 			state.total += addition(state, preciseStr)
-// 			break
-// 		case state.isSubtract:
-// 			state.total += subtraction(state, preciseStr)
-// 			break
-// 		case state.isMultiply:
-// 			state.total += multiplication(state, preciseStr)
-// 			break
-// 		case state.isDivide:
-// 			state.total += division(state, preciseStr)
-// 			break
-// 		default:
-// 			console.log("shouldn't see this...")
-// 	}
-// 	display.textContent = state.total
-// 	state.num1 = ''
-// 	state.num2 = ''
-// 	state.total = ''
-// 	state.isNum1Negative = false
-// 	state.isNum2Negative = false
-// 	state.isFirstDecimal = true
-// 	state.isNextNumber = false
-// 	state.isAdd = false
-// 	state.isSubtract = false
-// 	state.isMultiply = false
-// 	state.isDivide = false
-// })
+const clear = document.querySelector('.clear');
+clear?.addEventListener('click', () => {
+    window.location.reload();
+});
+/*
+const zero = document.querySelector('.zero')
+zero?.addEventListener('click', () => {
+    if (!state.isNextNumber) {
+        state.num1 += '0'
+        display.textContent = state.num1
+    } else {
+        state.num2 += '0'
+        display.textContent = state.num2
+    }
+
+    state.isFirstDecimal = true
+    state.isNextNumber = false
+})
+
+const plus = document.querySelector('.plus')
+plus?.addEventListener('click', () => {
+    state.isAdd = true
+    state.isNextNumber = true
+    state.isFirstDecimal = true
+})
+
+const equal = document.querySelector('.equal')
+equal?.addEventListener('click', () => {
+    switch (true) {
+        case state.isAdd:
+            state.total += addition(state, preciseStr)
+            break
+        case state.isSubtract:
+            state.total += subtraction(state, preciseStr)
+            break
+        case state.isMultiply:
+            state.total += multiplication(state, preciseStr)
+            break
+        case state.isDivide:
+            state.total += division(state, preciseStr)
+            break
+        default:
+            console.log("shouldn't see this...")
+    }
+
+    display.textContent = state.total
+
+    state.num1 = ''
+    state.num2 = ''
+    state.total = ''
+    state.isNum1Negative = false
+    state.isNum2Negative = false
+    state.isFirstDecimal = true
+    state.isNextNumber = false
+    state.isAdd = false
+    state.isSubtract = false
+    state.isMultiply = false
+    state.isDivide = false
+})
+*/
--- script.ts
+++ script.ts
@@ -5,27 +5,28 @@ function preciseStr(n: number) {
 	return n % 1 === 0 ? n.toString() : n.toPrecision(sigDig).toString()
 }
 
-function addition(_state: typeof state, fn: Function) {
-	const { num1, num2 } = _state
-	const sum = Number(num1) + Number(num2)
+function addition(_state: typeof state, fn: Function): string {
+	const { num1, total } = _state
+	const sum = Number(total) + Number(num1)
 	return fn(sum)
 }
 
-function subtraction(_state: typeof state, fn: Function) {
-	const { num1, num2 } = _state
-	const subtract = Number(num1) - Number(num2)
+function subtraction(_state: typeof state, fn: Function): string {
+	const { num1, total } = _state
+	const subtract = Number(total) - Number(num1)
 	return fn(subtract)
 }
 
-function multiplication(_state: typeof state, fn: Function) {
-	const { num1, num2 } = _state
-	const mult = Number(num1) * Number(num2)
+function multiplication(_state: typeof state, fn: Function): string {
+	const { num1, total } = _state
+	const mult = Number(total) * Number(num1)
 	return fn(mult)
 }
 
-function division(_state: typeof state, fn: Function) {
-	const { num1, num2 } = _state
-	const divide = Number(num2) === 0 ? 'lmao' : Number(num1) / Number(num2)
+function division(_state: typeof state, fn: Function): string {
+	const { num1, total } = _state
+	const divide =
+		Number(num1) === 0 || Number(total) === 0 ? '😲 😭' : Number(total) / Number(num1)
 	return fn(divide)
 }
 
@@ -33,16 +34,9 @@ const display = document.getElementById('display')
 
 const state = {
 	num1: '',
-	num2: '',
 	total: '',
-	isNum1Negative: false,
-	isNum2Negative: false,
 	isFirstDecimal: true,
-	isNextNumber: false,
-	isAdd: false,
-	isSubtract: false,
-	isMultiply: false,
-	isDivide: false,
+	isFirstOperator: true,
 }
 
 const zero = document.querySelector('.zero')
@@ -73,27 +67,61 @@ const eight = document.querySelector('.eight')
 eight?.addEventListener('click', () => {})
 
 const nine = document.querySelector('.nine')
-nine?.addEventListener('click', () => {})
+nine?.addEventListener('click', () => {
+	state.num1 += '9'
+})
 
 const dot = document.querySelector('.dot')
-dot?.addEventListener('click', () => {})
-
-const clear = document.querySelector('.clear')
-clear?.addEventListener('click', () => {
-	window.location.reload()
+dot?.addEventListener('click', () => {
+	if (state.isFirstDecimal) {
+		state.num1 += '.'
+		state.isFirstDecimal = false
+	}
 })
 
 const plus = document.querySelector('.plus')
-plus?.addEventListener('click', () => {})
+plus?.addEventListener('click', () => {
+	if (state.num1 === '.') {
+		display.textContent = '😲 😭'
+	} else {
+		state.total = addition(state, preciseStr)
+	}
+
+	state.num1 = ''
+})
 
 const subtract = document.querySelector('.minus')
-subtract?.addEventListener('click', () => {})
+subtract?.addEventListener('click', () => {
+	if (state.num1 === '.') {
+		display.textContent = '😲 😭'
+	} else {
+		state.total = subtraction(state, preciseStr)
+	}
+
+	state.num1 = ''
+})
 
 const mult = document.querySelector('.multiply')
-mult?.addEventListener('click', () => {})
+mult?.addEventListener('click', () => {
+	if (state.num1 === '.') {
+		display.textContent = '😲 😭'
+	} else {
+		state.total = multiplication(state, preciseStr)
+	}
+
+	state.num1 = ''
+})
 
 const divide = document.querySelector('.divide')
-divide?.addEventListener('click', () => {})
+divide?.addEventListener('click', () => {
+	if (state.num1 === '.') {
+		display.textContent = '😲 😭'
+	} else {
+		state.total = division(state, preciseStr)
+	}
+
+	state.num1 = ''
+})
 
 const plusMinus = document.querySelector('.plusMinus')
 plusMinus?.addEventListener('click', () => {})
@@ -104,57 +132,64 @@ percent?.addEventListener('click', () => {})
 const equal = document.querySelector('.equal')
 equal?.addEventListener('click', () => {})
 
-// const zero = document.querySelector('.zero')
-// zero?.addEventListener('click', () => {
-// if (!state.isNextNumber) {
-// state.num1 += '0'
-// display.textContent = state.num1
-// } else {
-// state.num2 += '0'
-// display.textContent = state.num2
-// }
-//
-// state.isFirstDecimal = true
-// state.isNextNumber = false
-// })
+const clear = document.querySelector('.clear')
+clear?.addEventListener('click', () => {
+	window.location.reload()
+})
 
-// const plus = document.querySelector('.plus')
-// plus?.addEventListener('click', () => {
-// 	state.isAdd = true
-// 	state.isNextNumber = true
-// 	state.isFirstDecimal = true
-// })
+/*	
+const zero = document.querySelector('.zero')
+zero?.addEventListener('click', () => {
+	if (!state.isNextNumber) {
+		state.num1 += '0'
+		display.textContent = state.num1
+	} else {
+		state.num2 += '0'
+		display.textContent = state.num2
+	}
 
-// const equal = document.querySelector('.equal')
-// equal?.addEventListener('click', () => {
-// 	switch (true) {
-// 		case state.isAdd:
-// 			state.total += addition(state, preciseStr)
-// 			break
-// 		case state.isSubtract:
-// 			state.total += subtraction(state, preciseStr)
-// 			break
-// 		case state.isMultiply:
-// 			state.total += multiplication(state, preciseStr)
-// 			break
-// 		case state.isDivide:
-// 			state.total += division(state, preciseStr)
-// 			break
-// 		default:
-// 			console.log("shouldn't see this...")
-// 	}
+	state.isFirstDecimal = true
+	state.isNextNumber = false
+})
 
-// 	display.textContent = state.total
+const plus = document.querySelector('.plus')
+plus?.addEventListener('click', () => {
+	state.isAdd = true
+	state.isNextNumber = true
+	state.isFirstDecimal = true
+})
 
-// 	state.num1 = ''
-// 	state.num2 = ''
-// 	state.total = ''
-// 	state.isNum1Negative = false
-// 	state.isNum2Negative = false
-// 	state.isFirstDecimal = true
-// 	state.isNextNumber = false
-// 	state.isAdd = false
-// 	state.isSubtract = false
-// 	state.isMultiply = false
-// 	state.isDivide = false
-// })
+const equal = document.querySelector('.equal')
+equal?.addEventListener('click', () => {
+	switch (true) {
+		case state.isAdd:
+			state.total += addition(state, preciseStr)
+			break
+		case state.isSubtract:
+			state.total += subtraction(state, preciseStr)
+			break
+		case state.isMultiply:
+			state.total += multiplication(state, preciseStr)
+			break
+		case state.isDivide:
+			state.total += division(state, preciseStr)
+			break
+		default:
+			console.log("shouldn't see this...")
+	}
+
+	display.textContent = state.total
+
+	state.num1 = ''
+	state.num2 = ''
+	state.total = ''
+	state.isNum1Negative = false
+	state.isNum2Negative = false
+	state.isFirstDecimal = true
+	state.isNextNumber = false
+	state.isAdd = false
+	state.isSubtract = false
+	state.isMultiply = false
+	state.isDivide = false
+})
+*/
